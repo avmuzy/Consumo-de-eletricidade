3d8243df0ede649c30bc7bea1e43719de18d9cf2
Andre Muzy <60109618+avmuzy@users.noreply.github.com>
2022-03-27 18:21:37 -0300

    pie chart

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pandas
 import pandas as pd
+import matplotlib.pyplot as plt
 
 x = pd.read_excel(r'C:\Users\Estacio\Desktop\CSV\Mentoria\consumo de luz\Planilha2.xlsx')
-print(x)
+plt.pie(x["APARELHO"], labels=x["POTÊNCIA (W)"], autopct="%1.0%%")
+plt.show(x)
