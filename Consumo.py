941f154ad0d6bc6abdbfc545ac9386f14b41a179
Andre Muzy <60109618+avmuzy@users.noreply.github.com>
2022-05-13 21:33:29 -0300

    Add files via upload

---
+++ Consumo.py
@@ -0,0 +1,9 @@
+print('*' * 40)
+print('     MEDIDOR DE CONSUMO ELETRICO')
+print('*' * 40)
+print('=' * 40)
+print('''MENU PRINCIPAL
+[1] selecione um aparelho:
+[2] inserir potncia/ potencia sugerida:
+[3] Horas de uso diaria: ''')
+print('=' * 40)
