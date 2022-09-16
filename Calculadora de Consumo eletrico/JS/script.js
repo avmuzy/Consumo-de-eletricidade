111a6434233cf3f9a735cb9dcfcac3a624cec54a
avms <andrevmuzy@gmail.com>
2022-09-16 19:36:48 -0300

    input del

--- Calculadora de Consumo eletrico/JS/script.js
+++ Calculadora de Consumo eletrico/JS/script.js
@@ -1,6 +1,6 @@
 function calcfridge() {
   var fridgeuse = document.getElementById('fridgeUse');
   var power = 300;
-  var res = (fridgeuse * power) / 1000;
-  console.log(res);
+  var result = (fridgeuse * power) / 1000;
+  console.log(result);
 }
