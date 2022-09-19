e0f26f72668a6a124a71c23e333957fcc345b2e6
avms <andrevmuzy@gmail.com>
2022-09-19 16:45:00 -0300

    Calculate on

--- Calculadora de Consumo eletrico/JS/script.js
+++ Calculadora de Consumo eletrico/JS/script.js
@@ -1,7 +1,35 @@
 function calcfridge() {
   var fridgeuse = document.getElementById('fridgeUse');
-  var power = 300;
-  var result = (fridgeuse * power) / 1000;
-  var res = document.getElementById('res');
-  res.innerHTML = `${result}`;
+  var fridgNum = Number(fridgeuse.value);
+  var result = (fridgNum * 300) / 1000;
+  var res1 = document.getElementById('res1');
+  res1.innerHTML = `${result}`;
+}
+function calctv() {
+  var tv = document.getElementById('tvUse');
+  var tvNum = Number(tv.value);
+  var result = (tvNum * 200) / 1000;
+  var res2 = document.getElementById('res2');
+  res2.innerHTML = `${result}`;
+}
+function calcac() {
+  var ac = document.getElementById('air_conditioningUse');
+  var acNum = Number(ac.value);
+  var result = (acNum * 1300) / 1000;
+  var res3 = document.getElementById('res3');
+  res3.innerHTML = `${result}`;
+}
+function calcMicroWave() {
+  var microwave = document.getElementById('hoursmicrowave');
+  var microNum = Number(microwave.value);
+  var result = (microNum * 750) / 1000;
+  var res4 = document.getElementById('res4');
+  res4.innerHTML = `${result}`;
+}
+function calcWashM() {
+  var washm = document.getElementById('washingMAchUse');
+  var washmNum = Number(washm.value);
+  var result = (washmNum * 1000) / 1000;
+  var res5 = document.getElementById('res5');
+  res5.innerHTML = `${result}`;
 }
