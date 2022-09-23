9a73abdc18dc12d89f34300b2e901fbd832af115
avms <andrevmuzy@gmail.com>
2022-09-23 19:22:52 -0300

    using <select>

--- Calculadora de Consumo eletrico/JS/script.js
+++ Calculadora de Consumo eletrico/JS/script.js
@@ -1,40 +1,40 @@
 function calcfridge() {
   var fridgeuse = document.getElementById('fridgeUse');
   var fridgNum = Number(fridgeuse.value);
-  var result = (fridgNum * 300) / 1000;
+  var result1 = (fridgNum * 300) / 1000;
   var res1 = document.getElementById('res1');
-  res1.innerHTML = `${result}`;
+  res1.innerHTML = `${result1}`;
 }
 function calctv() {
   var tv = document.getElementById('tvUse');
   var tvNum = Number(tv.value);
-  var result = (tvNum * 200) / 1000;
+  var result2 = (tvNum * 200) / 1000;
   var res2 = document.getElementById('res2');
-  res2.innerHTML = `${result}`;
+  res2.innerHTML = `${result2}`;
 }
 function calcac() {
   var ac = document.getElementById('air_conditioningUse');
   var acNum = Number(ac.value);
-  var result = (acNum * 1300) / 1000;
+  var result3 = (acNum * 1300) / 1000;
   var res3 = document.getElementById('res3');
-  res3.innerHTML = `${result}`;
+  res3.innerHTML = `${result3}`;
 }
 function calcMicroWave() {
   var microwave = document.getElementById('hoursmicrowave');
   var microNum = Number(microwave.value);
-  var result = (microNum * 750) / 1000;
+  var result4 = (microNum * 750) / 1000;
   var res4 = document.getElementById('res4');
-  res4.innerHTML = `${result}`;
+  res4.innerHTML = `${result4}`;
 }
 function calcWashM() {
   var washm = document.getElementById('washingMAchUse');
   var washmNum = Number(washm.value);
-  var result = (washmNum * 1000) / 1000;
+  var result5 = (washmNum * 1000) / 1000;
   var res5 = document.getElementById('res5');
-  res5.innerHTML = `${result}`;
+  res5.innerHTML = `${result5}`;
 }
 function total() {
-  var daily = res1 + res2 + res3 + res4 + res5;
+  var daily = result1 + result2 + result3 + result4 + result5;
   var dailyNum = Number(daily.value);
   var dailyres = document.getElementById('daily');
   dailyres.innerHTML = `${dailyNum}`;
